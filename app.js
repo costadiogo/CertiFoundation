5daa58dfea4f3dc9f657b86b44622fb88d78f14f
DIogo Costa <costadiogo30@gmail.com>
2021-02-16 15:09:04 -0300

    Update main route

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
   
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/html; charset=utf-8');
-  res.end('<h1> Challenger Certi</h1> <p> Insira o numero após htttp://localhost:3000/ na barra de endereço.</p> <p>Ex: http://localhost:3000/10 </p> ');
+  res.send({message: 'Welcome to challenger of Certi Foundation'});
+
 });
 
 app.get('/:id', (req, res) =>{
