6ee1f75bb0ed134d8238c73184fd85a0ae5ff3c4
DIogo Costa <costadiogo30@gmail.com>
2021-02-16 14:24:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const extenso = require('./controllers/extenso');
 
 const router = express.Router();
 
 app.use(router);
 
-app.get('/', (req, res) => {
-    
-    res.send({message: 'Welcome to Challenger of Certi Foundation'});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) =>{
+  
+  res.statusCode = 200;
+  res.setHeader('content-type', 'text/html; charset=utf-8');
+  res.end('<h1> Challenger Certi</h1> <p> Insira o numero após htttp://localhost:3000/ na barra de endereço.</p> <p>Ex: http://localhost:3000/10 </p> ');
 });
+
+app.get('/:id', (req, res) =>{
+
+  let value = req.params.id;
+  res.statusCode = 200;
+  res.setHeader('content-type', 'application/json');
+  
+  extenso.transform(res, value);
+
+});
+
 
 
 module.exports = app;
